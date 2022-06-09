1293e650ef14d31aa4f437585de9780baffa6251
simonsimon9 <simonkuang9@outlook.com>
2022-06-08 20:39:32 -0400

    ng Style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,41 +1,11 @@
 import { Component } from '@angular/core';
 @Component({
-    selector: 'app-root',
-    template: `<input type="checkbox" [(ngModel)]="doesNotExercise"
-    (change)="adjustCSS()"/>Does not exercise.
-    <input type="checkbox" [(ngModel)]="over55" (change)="adjustCSS()"/>
-    Is over 55.
-    <div *ngIf="doesNotExercise && over55" [ngClass]="myClasses" >
-    Is at risk of heart disease.</div>
-    <div *ngIf="doesNotExercise && !over55" [ngClass]="myClasses" >
-    Fat</div>
-    <div *ngIf="!doesNotExercise && over55" [ngClass]="myClasses" >
-    Old</div>
-    `
-    ,
-    styles: [`
-          .warning {
-              font-weight:bold;
-          }
-          .danger {
-              color:red;
-          }
-          `
-    ]
-    })
-    export class AppComponent {
-      title = 'app';
-      doesNotExercise = false;
-      over55 = false;
-
-      myClasses = {
-        warning: false,
-        danger: false
-      }
-
-      adjustCSS() {
-       
-        this.myClasses.warning =true;
-        this.myClasses.danger = true;
-      }
-    }
+selector: 'app-root',
+template: `<p [ngStyle]="myStyles">stye binding example</p>`,
+})
+export class AppComponent {
+myStyles = {
+'color':'red',
+'font-weight':'bold'
+}
+}
