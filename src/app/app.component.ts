c304f481788d55b63d8ee9c9e257c151eac0aed7
simonsimon9 <simonkuang9@outlook.com>
2022-06-08 20:18:06 -0400

    multiple css class binding with [ngClass]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,29 @@
 import { Component } from '@angular/core';
 @Component({
-selector: 'app-root',
-template: `<label [class.danger]="foodWarning">Has food allergy.</label>
-<input type="checkbox" [(ngModel)]="foodWarning" />
-`,
-styles: [`
-.danger {
-color:orange;
-font-weight:bold;
-}`
-]
+    selector: 'app-root',
+    template: `
+    <label [class.danger]="foodWarning">Has food allergy.</label>
+    <input type="checkbox" [(ngModel)]="foodWarning" />
+    <label [class.green]="greenWarning">Vegges</label>
+    <input type="checkbox" [(ngModel)]="greenWarning"/>
+    `,
+    styles: [`
+              .danger {
+                  color:orange;
+                  font-weight:bold;
+               }
+               .green{
+                 color: green;
+                 font-weight: bold;
+               }`
+            ]
 })
 export class AppComponent {
 foodWarning:Boolean
+greenWarning:Boolean
 constructor() {
-// Set default value to false.
-this.foodWarning = false;
+  // Set default value to false.
+  this.foodWarning = false;
+  this.greenWarning = false;
 }
 }
