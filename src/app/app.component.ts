807f7a7f4cb1aad2f5f6d359f2ac0fbcb425ee06
simonsimon9 <simonkuang9@outlook.com>
2022-06-08 20:40:32 -0400

    binding one style at a time [style]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
 @Component({
 selector: 'app-root',
-template: `<p [ngStyle]="myStyles">stye binding example</p>`,
+template: `<p [style.font-weight]="myWeight">style binding example</p>`,
 })
 export class AppComponent {
-myStyles = {
-'color':'red',
-'font-weight':'bold'
-}
+myWeight = "bold";
 }
